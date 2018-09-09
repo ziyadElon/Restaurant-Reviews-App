1ea548278b2ccc8191a3fb9ced176cb9e86aef89
Ziyad <ziyad.arfan@yahoo.com>
2018-09-09 21:54:42 +0530

    service worker added with cache functionality

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -125,17 +125,20 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  const div = document.createElement('div');
   const name = document.createElement('p');
   name.innerHTML = review.name;
-  li.appendChild(name);
+  div.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  div.appendChild(date);
+  li.appendChild(div);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
-  li.appendChild(rating);
+  rating.className = 'rating';
+  li.appendChild(rating);  
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
--- sw.js
+++ sw.js
@@ -1,5 +1,50 @@
 console.log('Inside SW script');
-//
+let currentCacheName = 'restaurants-v3';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(currentCacheName).then(function(cache) {
+           return cache.addAll([
+            '/',
+            'js/main.js',
+            'js/dbhelper.js',
+            'js/restaurant_info.js',
+            'css/styles.css',
+            'data/restaurants.json',
+            '/img',
+            '/key.js'
+            //'//normalize-css.googlecode.com/svn/trunk/normalize.css'
+            ]);        
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter( function(cacheName) {
+                    return cacheName.startsWith('restaurants') && 
+                            cacheName != currentCacheName 
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );    
+});
+
 self.addEventListener('fetch', function(event) {
-    console.log('cghfh');
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if(response) return response;
+            return fetch(event.request)
+            .then(dynResponse => {
+                return caches.open(currentCacheName).then(cache => {
+                  cache.put(event.request.url, dynResponse.clone());
+                  return dynResponse;
+                });
+              });
+        }) 
+    );
 });
